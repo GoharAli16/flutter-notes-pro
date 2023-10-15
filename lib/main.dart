07becc8378989bbdbadd0f315215925fcc45577e
Gohar Ali <goharali16@gmail.com>
2023-10-15 11:30:00 +0500

    Initial setup: Advanced Flutter notes app
    - Create comprehensive note-taking application with modern architecture
    - Set up Riverpod state management and Firebase integration
    - Add cloud synchronization with real-time updates
    - Implement rich text editor with markdown support
    - Configure AI-powered note organization and search
    - Add voice-to-text and text-to-speech functionality
    - Include collaborative features and sharing capabilities
    - Set up encryption for sensitive notes and data
    
    Core Features:
    - Rich text editor with formatting options
    - Cloud sync across multiple devices
    - AI-powered note categorization and search
    - Voice notes and speech recognition
    - Note templates and organization
    - Collaborative editing and sharing
    - End-to-end encryption for privacy
    - Offline support with sync when online

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'core/config/app_config.dart';
+import 'core/theme/app_theme.dart';
+import 'core/routing/app_router.dart';
+import 'core/services/notification_service.dart';
+import 'core/services/sync_service.dart';
+import 'core/services/ai_service.dart';
+import 'features/auth/presentation/providers/auth_provider.dart';
+import 'features/notes/presentation/providers/notes_provider.dart';
+import 'features/folders/presentation/providers/folders_provider.dart';
+import 'features/search/presentation/providers/search_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Initialize Firebase
+  await Firebase.initializeApp();
+  
+  // Initialize Hive
+  await Hive.initFlutter();
+  
+  // Initialize services
+  await NotificationService.initialize();
+  await SyncService.initialize();
+  await AIService.initialize();
+  
+  runApp(
+    const ProviderScope(
+      child: NotesProApp(),
+    ),
+  );
+}
+
+class NotesProApp extends ConsumerWidget {
+  const NotesProApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(routerProvider);
+    final themeMode = ref.watch(themeModeProvider);
+    
+    return MaterialApp.router(
+      title: AppConfig.appName,
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
+      routerConfig: router,
+    );
+  }
+}
